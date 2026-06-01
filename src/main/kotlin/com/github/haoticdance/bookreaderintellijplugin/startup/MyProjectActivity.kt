43ea44e2e4febe4cc1e2f311cc918d7c952325d7
GitHub Action <action@github.com>
2026-06-01 11:27:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/haoticdance/bookreaderintellijplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/haoticdance/bookreaderintellijplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.haoticdance.bookreaderintellijplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
